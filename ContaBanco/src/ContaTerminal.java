812a9e171635e78ce83067eab5a6619758ba6bd1
eliances <eliancesar.p@gmail.com>
2024-05-28 18:17:43 -0300

    Criação da interface para adquirir informações do usuário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá! Por favor, insira seu primeiro nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Agora insira seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Perfeito! agora insira sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Insira o numero de sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Para finalizar insira o saldo em sua conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
